f4aa64df63ce66c37dcda4b8aa95cf6f2d1023b0
Patrick <pa@arthurfamily.ws>
2016-06-27 20:18:03 -0400

    minor refactor

--- prefill.rb
+++ prefill.rb
@@ -1,14 +1,14 @@
 # method that prefills and array of n with v values,
 # if n is not a number or negative return invalid
 
-def prefill(n, v)
+def prefill(n, v=nil)
   if (n =~ /\d/ || (n.is_a?Integer)) && n.to_i >= 0
     array = Array.new(n.to_i)
     array.each_index do |ind|
       array[ind] = v
     end
   else
-    "#{n} is invalid"
+    raise TypeError, "#{n} is invalid"
   end
 end
 
